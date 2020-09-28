898eaaaaa5e2e107825062e242530c096992d3f1
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-09-28 10:41:26 +0800

    passes test_line

--- adv.py
+++ adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-
+from collections import deque
 import random
 from ast import literal_eval
 
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,6 +29,55 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# print(f"line 48: {player.current_room.id}")
+# print(f"line 49: {player.current_room.get_exits()}")
+# print(f"line 50: {player.travel(traversal_path[0])}")
+
+def bfs(self, starting_vertex):
+        visited = set()
+        queue = deque()
+        queue.append([starting_vertex])
+
+        while len(queue) > 0:
+            current_path = queue.popleft()
+            current_node = current_path[-1]
+
+            if current_node == "?":
+                return current_path
+            
+            if current_node not in visited:
+                visited.add(current_node)
+                for neighbor in player.current_room.get_exits():
+                    new_path = list(current_path)
+                    new_path.append(neighbor)
+                    queue.append(new_path)
+
+
+graph = {}
+stack = deque()
+stack.append(player.current_room.id)
+
+while len(stack) > 0:
+    current_loc = stack.pop()
+    if current_loc not in graph:
+        graph[current_loc] = {}
+        for path in player.current_room.get_exits():
+            graph[current_loc][path] = "?"
+
+    for k, v in graph[current_loc].items():
+        if v == "?":
+            player.travel(k)
+            traversal_path.append(k)
+            graph[current_loc][k] = player.current_room.id
+            if player.current_room.id not in graph:
+                stack.append(player.current_room.id)
+                break
+            else:
+                continue
+
+
+print(traversal_path)
+print(graph)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -51,12 +100,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
